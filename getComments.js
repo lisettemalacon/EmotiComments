2c151587c91e38633a215cfc4070cd2d8f044ed1
Lisette Malacon <lmalacon123@gmail.com>
2023-02-22 23:33:56 -0800

    add bare script file

---
+++ getComments.js
@@ -0,0 +1,4 @@
+// id=contents is a div with a whole bunch of comments.
+const comment = document.getElementById("comment");
+const commentContent = document.getElementById("comment-content")
+console.log(commentContent.textContent);
