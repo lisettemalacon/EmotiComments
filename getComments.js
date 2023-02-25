2c8552fd4a0d9ddd06cf102116104a5fc72225eb
Lisette Malacon <lmalacon123@gmail.com>
2023-02-24 22:01:57 -0800

    click on blurred comment for modal with buttons to pop up (very bare bones)

--- getComments.js
+++ getComments.js
@@ -7,21 +7,49 @@ for (let i = 0; i < commentContents.length; i++) {
 }
 
 const commentThreads = document.querySelectorAll("[id='content-text']");
+const fullDiv = document.querySelector('ytd-app');
 // const contentDiv = document.querySelectorAll("[id='content']")
 for (let i = 0; i < commentThreads.length; i++) {
     const commentThread = commentThreads[i];
     commentThread.style.position = 'relative';
     const newDiv = document.createElement("div");
     newDiv.style.backdropFilter = 'blur(5px)';
-    newDiv.style.width = '900px';
+    newDiv.style.width = '920px';
     newDiv.style.height = '300px';
     newDiv.style.position = 'absolute';
     
-    // click to unblur
     newDiv.addEventListener('click', function() {
-        commentThread.removeChild(commentThread.firstChild)
+        const modal = document.createElement('div');
+        modal.style.backgroundColor = 'white';
+        modal.style.width = '300px';
+        modal.style.height = '300px';
+        modal.style.position = 'fixed';
+        modal.style.left = '50%';
+        modal.style.top = '50%';
+        modal.style.transform = 'translate(-50%, -50%)';
+
+        const revealContent = document.createElement('h2');
+        revealContent.style.color = 'black';
+        revealContent.textContent = "Reveal comment";
+        modal.appendChild(revealContent);
+
+        const yesButton = document.createElement('button');
+        yesButton.textContent = "YES, REVEAL";
+        yesButton.addEventListener("click", function() {
+            commentThread.removeChild(commentThread.firstChild);
+            commentThread.removeChild(modal);
+        })
+        const noButton = document.createElement('button');
+        noButton.textContent = "NO, CANCEL";
+        noButton.addEventListener("click", function() {
+            commentThread.removeChild(modal);
+        })
+        modal.appendChild(noButton);
+        modal.appendChild(yesButton);
+        // fullDiv.insertBefore(modal, fullDiv.firstChild);
+        commentThread.appendChild(modal);
+
     })
     commentThread.insertBefore(newDiv, commentThread.firstChild);
-    
 }
 
